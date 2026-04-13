cc5848b9d80884cf91bd35a007e012d28fef914e
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-04-13 18:37:35 +0530

    useLocation use

--- react-router-dom/src/component/User.jsx
+++ react-router-dom/src/component/User.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation  } from 'react-router-dom'
 
 function User() {
     const params = useParams()
     const {name} = params;
     console.log(name)
+    const location = useLocation();
+    console.log(location);
     return (
         <>
         {/* <h1>User Component</h1> */}
