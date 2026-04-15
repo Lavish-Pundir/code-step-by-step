8bd0f37405977cd478a0234dfe72578fe746e1b8
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-04-15 11:48:39 +0530

    state with class outstanding

--- previous-state/src/main.jsx
+++ previous-state/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Previous from './Previous.jsx'
+import StateWithObject from './StateWithObject.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Previous />
+    <StateWithObject />
   </StrictMode>,
 )
